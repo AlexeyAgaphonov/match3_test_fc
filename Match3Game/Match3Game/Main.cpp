cd9161795d648f74ed12025ac5cbcfed37cda9e5
Alexey <alexeyagaphonov@gmail.com>
2021-06-10 21:01:52 +0300

    SFML-2.5.1 init as main Graphics framework

---
+++ Match3Game/Match3Game/Main.cpp
@@ -0,0 +1,57 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Image.hpp>
+
+const constexpr int32_t FramesPerSecond = 60;
+const constexpr int32_t TicksDelay = 1000 / FramesPerSecond;
+
+
+class Application
+{
+public:
+	Application()
+	{
+		_window.reset(new sf::RenderWindow(sf::VideoMode(1024, 768), "Match3 Game"));
+		texture1.loadFromFile("bg.png");
+		sprite.setTexture(texture1);
+	}
+
+	void HandleUpdate()
+	{
+		sf::Event ev;
+
+		sf::Clock deltaClock;
+		while (_window->isOpen())
+		{
+			const auto deltaTime = deltaClock.restart();
+			while (_window->pollEvent(ev))
+			{
+				if (ev.type == sf::Event::Closed)
+				{
+					_window->close();
+				}
+			}
+
+			_window->clear();
+			_window->draw(sprite);
+			_window->display();
+
+			if (deltaTime.asMilliseconds() < TicksDelay)
+			{
+				sf::sleep(sf::milliseconds(TicksDelay) - deltaTime);
+			}
+		}
+	}
+
+private:
+	sf::Texture texture1;
+	sf::Sprite sprite;
+	std::unique_ptr<sf::RenderWindow> _window;
+};
+
+
+int main()
+{
+	Application app;
+	app.HandleUpdate();
+	return 0;
+}
